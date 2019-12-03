0f679fbd2aaa7656272d3a0df2be6c936f9f9eff
Hywel Stoakes <h.stoakes@auckland.ac.nz>
2019-12-03 13:44:21 +1100

    file paths

--- Code/01_emu_basics.R
+++ Code/01_emu_basics.R
@@ -1,23 +1,26 @@
 # Be sure to read the Readme.Rmd document first
 # All materials are available at: https://github.com/Hywel-Stoakes/EmuRSummerSchool
 # An Important resource is the Emu Manual https://ips-lmu.github.io/The-EMU-SDMS-Manual/ by Raphael Winkelmann
+#### Introduction ####
 require(emuR)
-## Not run: 
-# create demo data including an emuDB called "ae" 
+## Not run:
+# create demo data including an emuDB called "ae"
 # for a local project change the path (each folder inside its own parentheses)
-create_emuRdemoData(dir = file.path("/","cloud","project","DATA"))
+create_emuRdemoData(dir = file.path("DATA"))
 
 # construct path to demo emuDB
-path2ae = file.path("/cloud/project/DATA", "emuR_demoData", "ae_emuDB")
+# On Rstudio Cloud
+# path2ae = file.path("/cloud/project/DATA", "emuR_demoData", "ae_emuDB")
+# Local (From Github)
+path2ae = file.path("DATA", "emuR_demoData", "ae_emuDB")
 
 # load emuDB into current R session
 ae = load_emuDB(path2ae)
-serve(ae) # This command doesn't work Rstudio cloud
-# query loaded emuDB
+serve(ae) # This command doesn't work Rstudio cloud - locally this will take a little time
+# query loaded emuDBh
 lvowels = query(ae, "Phonetic = i: | u: | o:")
 
-# extract labels from query result 
-#### NEED to Update this function ####
+# extract labels from query result
 label <- function(x){
   x$labels
 }
@@ -26,6 +29,7 @@ lvowels.labs = label(lvowels)
 
 # list all ssffTrackDefinitions of emuDB
 list_ssffTrackDefinitions(ae)
+summary(ae)
 
 # get formant trackdata defined in ssffTrackDefinitions "fm" for query result
 # extract track values at temporal midpoint of segments
